d0f2eb74ae91e1d6766fdf608cdfb732dd2376b3
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-14 23:59:33 +0300

    -Добавлен интерфейс сервиса домашки

---
+++ src/ConstructionUniversity.Domain/Homeworks/IHomeworkService.cs
@@ -0,0 +1,10 @@
+﻿namespace ConstructionUniversity.Domain.Homeworks;
+
+public interface IHomeworkService
+{
+    Task<Homework> NewAsync(Homework model);
+    Task<Homework?> GetAsync(Guid id);
+    Task<IReadOnlyCollection<Homework>> GetAllAsync();
+    Task<Homework> UpdateAsync(Guid id, Homework model);
+    Task<bool> DeleteAsync(Guid id);
+}
