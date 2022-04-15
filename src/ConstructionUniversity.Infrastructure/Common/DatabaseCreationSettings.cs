074a7018ae302b21ce792dcc59bd16e0e9be7eb8
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-15 22:46:24 +0300

    -Добавлена моделька с настройками создания базы

---
+++ src/ConstructionUniversity.Infrastructure/Common/DatabaseCreationSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace ConstructionUniversity.Infrastructure.Common;
+
+public class DatabaseCreationSettings
+{
+    public bool ReCreateDatabaseAtFirstStartup { get; set; }
+    public bool CreateDatabaseWithTestData { get; set; }
+}
