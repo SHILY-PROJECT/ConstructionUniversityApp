15972b1227ff972f905218f42da0f09c6178a463
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-16 16:51:17 +0300

    -Заимплеменчены репозитории: домашки, учителя, студента

---
+++ src/ConstructionUniversity.Infrastructure/Homeworks/HomeworkRepository.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ConstructionUniversity.Infrastructure.Common;
+using ConstructionUniversity.Domain.Homeworks;
+using ConstructionUniversity.Infrastructure.DataAccess;
+
+namespace ConstructionUniversity.Infrastructure.Homeworks;
+
+internal class HomeworkRepository : BaseRepository<Homework, HomeworkDb>, IHomeworkRepository
+{
+    private readonly ConstructionUniversityDbContext _context;
+    private readonly IMapper _mapper;
+
+    public HomeworkRepository(ConstructionUniversityDbContext context, IMapper mapper) : base(context, mapper, context.Homeworks)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+}
