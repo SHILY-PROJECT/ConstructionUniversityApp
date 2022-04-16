f0a6c992e3b0414e2ccfc918c69eee2af0e31b65
Ilya Shimbuev <iliyarock@gmail.com>
2022-04-16 17:47:10 +0300

    -Добавлены валидаторы для dto'шек

---
+++ src/ConstructionUniversity.WebApi/Models/Teachers/Validators/TeacherDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using ConstructionUniversity.WebApi.Models.Teachers;
+
+namespace ConstructionUniversity.WebApi.Models.Teachers.Validators;
+
+public class TeacherDtoValidator : AbstractValidator<TeacherDto>
+{
+    public TeacherDtoValidator()
+    {
+        RuleFor(x => x.FirstName).Length(2, 50);
+        RuleFor(x => x.LastName).Length(2, 50);
+        RuleFor(x => x.Email).Length(2, 170).EmailAddress();
+    }
+}
